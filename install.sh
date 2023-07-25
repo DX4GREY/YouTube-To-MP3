08d0fd7fb304591a655eb82ecb58587e61244cd1
dx4grey <dx4mal@gmail.com>
2023-07-25 21:07:08 +0700

    Push

---
+++ install.sh
@@ -0,0 +1,36 @@
+install_termux() {
+    pkg update
+    pkg upgrade
+    pkg install git
+    git clone https://github.com/DX4GREY/YouTube-To-MP3
+    cd FaAng-Toolkit
+    sh setup.sh
+    cd ../
+    rm -rf YouTube-To-MP3
+}
+install_linux() {
+    sudo apt-get update
+    sudo apt-get upgrade
+    sudo apt-get install git
+    git clone https://github.com/DX4GREY/YouTube-To-MP3
+    cd YouTube-To-MP3
+    sh setup.sh
+    cd ../
+    sudo rm -rf YouTube-To-MP3
+}
+
+detect_platform() {
+    platform=$(uname -o)
+    case $platform in
+        "Android")
+            install_termux
+            ;;
+        "GNU/Linux")
+            install_linux
+            ;;
+        *)
+            echo "Unsupported platform: $platform"
+            ;;
+    esac
+}
+detect_platform
